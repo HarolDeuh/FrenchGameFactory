4903faa0dbf0073f93dd2cb16b1b15b50aba4781
Haroldeuh <harold.aholoukpe@gmail.com>
2019-11-29 22:52:26 +0100

    ajout projet du jeu command line XCode

---
+++ TheGame/TheGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TheGame
+//
+//  Created by harold aholoukpe on 29/11/2019.
+//  Copyright © 2019 harold aholoukpe. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
