a8d34548cd1795d9417106800ccddbbba0cc350c
Haroldeuh <harold.aholoukpe@gmail.com>
2019-12-01 19:16:26 +0100

    fichier main clean

--- TheGame/TheGame/PlayerClass.swift
+++ TheGame/TheGame/PlayerClass.swift
@@ -15,4 +15,26 @@ class Player {
     init(name: String) {
         self.name = name
     }
+    
+    func choixPokemon(pokemon: String) -> String {
+        
+        switch pokemon {
+        case "pikachu":
+            self.team.append(pikachu)
+        case "roucool":
+            self.team.append(roucool)
+        case "magicarpe":
+            self.team.append(magicarpe)
+        case "reptincel":
+            self.team.append(reptincel)
+        case "abra":
+            self.team.append(abra)
+        case "ronflex":
+            self.team.append(ronflex)
+        default:
+            print("rien a ajouter")
+        }
+        
+        return pokemon
+    }
 }
--- TheGame/TheGame/main.swift
+++ TheGame/TheGame/main.swift
@@ -8,85 +8,78 @@
 
 import Foundation
 
-// On initialise le nouveau jeu
-var state: GameState = .NewGame
+// MARK: variables et fonctions pour lancer le jeu
 
 // On crée les 2 joueurs de la partie
-print("Bonjour.")
-
 print("Joueur 1, veuillez entrer votre prenom")
 let prenomUn = readLine(strippingNewline: true)!
-
 print("Joueur 2, veuillez entrer votre prenom")
 let prenomDeux = readLine(strippingNewline: true)!
-
 
 //Création des objets Player
 let monJoueurUn = Player(name: prenomUn)
 let monJoueurDeux = Player(name: prenomDeux)
-//print(monJoueurUn.team)
 
-func debutPartie() {
-    print("Vous êtes le joueur 1. Votre nom est \(monJoueurUn.name)")
-    print("Vous êtes le joueur 2. Votre nom est \(monJoueurDeux.name)")
+
+// Methode qui permet de créer les équipes des joueurs. 3 Pokémons par joueur
+func createTeam(joueur: Player) {
+    print("\(joueur.name), À votre tour")
     
     for choice in 1...3 {
         print("Choisissez les pokémons de votre équipe (encore \(4 - choice))")
         let pokemon = readLine()!
-        choixPokemon(pokemon: pokemon)
+        //choixPokemon(pokemon: pokemon)
+        monJoueurUn.choixPokemon(pokemon: pokemon)
+        
     }
-    
-    state = .InGame
-    milieuPartie()
+    print(separateur)
 }
 
-func milieuPartie() {
-    
-    //coupDeBoule(attaquant: monJoueurUn.team[pikachu], defense: monJoueurDeux.team[roucool])
-    
-    //let pokemon = readLine()!
-    //choixPokemon(pokemon: pokemon)
-    
-    
- 
-    print(monJoueurUn.team)
-    
-    
-    
-    
-    
-}
-
-func choixPokemon(pokemon: String) -> String {
-    // le code
-    print("Entrez le nom de vos persos")
-    switch pokemon {
-    case "pikachu":
-        monJoueurUn.team.append(pikachu)
-    case "roucool":
-        monJoueurUn.team.append(roucool)
-    case "magicarpe":
-        monJoueurUn.team.append(magicarpe)
-    case "reptincel":
-        monJoueurUn.team.append(reptincel)
-    case "abra":
-        monJoueurUn.team.append(abra)
-    case "ronflex":
-        monJoueurUn.team.append(ronflex)
-    default:
-        print("rien a ajouter")
-    }
-    
-    return pokemon
-}
-
-
+//Methode pour attaquer
 func coupDeBoule(attaquant: Character, defense: Character) {
     
     defense.vie = defense.vie -  attaquant.weapon.attaque
     print(defense.vie)
 }
 
+
+// MARK: variables texte console
+var separateur = "------------------------------"
+var separateur2 = "=============================="
+
+
+
+
+// MARK: On lance la partie
+// On initialise le nouveau jeu
+
+
+
+func debutPartie() {
+    print("Joueur 1: \(monJoueurUn.name)")
+    print("Joueur 2: \(monJoueurDeux.name)")
+    print(separateur)
+
+    createTeam(joueur: monJoueurUn)
+    createTeam(joueur: monJoueurDeux)
+    
+    milieuPartie()
+}
+    
+
+func milieuPartie() {
+    print(separateur2)
+    print(monJoueurUn.team.count)
+    print(monJoueurDeux.team.count)
+    
+    
+}
+
+func finPartie() {
+    print("Fin de match")
+}
+
+var state: GameState = .NewGame
 switch state {
     
 case .NewGame:
@@ -94,5 +87,17 @@ case .NewGame:
 case .InGame:
     milieuPartie()
 case .EndGame:
-    print("Le code")
+    finPartie()
 }
+
+
+/*
+ pokemons dispo pour le jeu :
+ 
+ pikachu
+ roucool
+ magicarpe
+ reptincel
+ abra
+ ronflex
+ */
